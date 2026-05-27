2eee2b6e1d378abc553e48ae9da7abf244ae16af
Andrey Prokhorenko <_@example.com>
2026-05-27 18:13:48 +0500

    prepare backend for production

--- server/index.js
+++ server/index.js
@@ -591,7 +591,7 @@ app.post("/api/upload", verifyAdmin, upload.array("files"), (req, res) => {
 
     type: file.mimetype.startsWith("video") ? "video" : "image",
 
-    src: `http://localhost:${PORT}/uploads/${file.filename}`,
+    src: `${process.env.SERVER_URL}/uploads/${file.filename}`,
 
     name: file.originalname,
   }))
