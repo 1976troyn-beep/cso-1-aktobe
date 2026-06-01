a6c9cccee745c6a529ef7009ca06b16d20a44fd4
Andrey Prokhorenko <_@example.com>
2026-06-01 18:41:22 +0500

    Add Vercel Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Analytics } from "@vercel/analytics/react"
 
 import App from "./App.jsx"
 
@@ -20,5 +21,6 @@ window.history.scrollRestoration =
 createRoot(rootElement).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 )
