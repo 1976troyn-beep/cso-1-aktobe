276f30d93143fdf261b4aab3500146da02decf4b
Andrey Prokhorenko <_@example.com>
2026-06-01 19:26:34 +0500

    Add Vercel Speed Insights

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import App from "./App.jsx"
 
@@ -22,5 +23,6 @@ createRoot(rootElement).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>
 )
